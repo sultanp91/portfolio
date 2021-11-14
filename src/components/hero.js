a84e035b7c840250278642c632fc31bfd9cad5ee
Sultan Parbes <sultanp@gmail.com>
2021-11-14 14:49:05 +0000

    initial styling/content on hero section

---
+++ src/components/hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Sultan Parbes</h1>
+      <h2>Front End Developer</h2>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <ul className="nav-links">
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
