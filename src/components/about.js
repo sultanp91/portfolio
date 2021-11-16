cdf00adc5ab192414d16ffa62017a2b059598536
Sultan Parbes <sultanp@gmail.com>
2021-11-16 00:52:02 +0000

    updated about section

--- src/components/about.js
+++ src/components/about.js
@@ -5,22 +5,12 @@ function About() {
     <section className="about-section" id="about">
       <h2 className="about-section__heading">About Me</h2>
       <p className="about-section__content">
-        I am a Front End developer from London. I am currently working through
-        the full stack JavaScript course at The Odin Project, an open source
-        project based curriculum. I enjoy taking complex problems, breaking them
-        down into smaller problems and engineering understandable and performant
-        solutions I have built mutiple projects with HTML, CSS and JavaScript -
-        each project developing my understanding of JavaScript as a language and
-        how to leverage it to create interactive experiences. I am confident
-        with modern ES6 syntax, working with async code and have a solid
-        understanding of JavaScript fundamentals such as scope, closure and
-        objected oriented programming. I am passionate about React having built
-        projects using both class and functional components with hooks. I am
-        proficient with Git and modern tooling such as WebPack, NPM, ESLint and
-        Prettier. My goal is to become a full stack developer and each day I
-        become closer to that goal by learning and building every opportunity I
-        get. I am currently looking for opportunities in web development. If you
-        wish to know more about me please do not hesitate to ask.
+        I am a front end developer from London. I enjoy building web apps using
+        JavaScript and React. I am confident in writing semantic HTML and
+        styling pages using CSS or SASS. I am able to use Git and GitHub for
+        version control and know my way around the command line. I also have
+        experience in using modern tooling and techniques such as WebPack, NPM,
+        ESLint, TDD etc. I am currently looking for roles in web development.
       </p>
     </section>
   );
