98cf0209bb08985e665c91635203490a6b4dfa83
Sultan Parbes <sultanp@gmail.com>
2021-11-19 23:34:10 +0000

    updated styling on carousel

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,9 +4,15 @@ import ImageCarousel from "./carousel";
 function Projects({ title, description, images }) {
   return (
     <div className="projects-section__item">
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <ImageCarousel images={images} title={title} />
+      <div className="projects-section__text">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+      <ImageCarousel
+        className="projects-section__carousel"
+        images={images}
+        title={title}
+      />
     </div>
   );
 }
