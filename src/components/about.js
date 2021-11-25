e30453f71ccbf973e58c5800fa365161fbf2777e
Sultan Parbes <sultanp@gmail.com>
2021-11-25 19:26:59 +0000

    updated about section

--- src/components/about.js
+++ src/components/about.js
@@ -5,16 +5,16 @@ function About() {
     <section className="about-section" id="about">
       <h2 className="about-section__heading heading-text-gradient">About Me</h2>
       <p className="about-section__content">
-        I'm a web developer from London. I enjoy building web apps using
-        JavaScript and React. I'm confident in writing modern JavaScript code,
-        HTML and styling using either plain CSS or SASS. I am proficient in
-        using Git and GitHub and know my way around the command line. I also
+        I'm a front end web developer from London. I enjoy building web apps
+        using JavaScript and React. I'm confident in writing modern JavaScript
+        code, HTML and styling using either plain CSS or SASS. I am proficient
+        in using Git and GitHub and know my way around the command line. I also
         have experience in using modern tooling and techniques such as WebPack,
-        NPM, ESLint and TDD using Jest. I am also currently learning Node,
-        Express and MongoDB.
+        NPM, ESLint and test driven development using Jest. I am also learning
+        backend development with Node, Express and MongoDB.
       </p>
       <p className="about-section__content">
-        I am currently looking for roles in front end development.
+        I am currently looking for roles in web development.
       </p>
     </section>
   );
