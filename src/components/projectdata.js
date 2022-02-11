ffd7dc611009c8791b5e7920ede1f82c6aec1707
Sultan Parbes <sultanp@gmail.com>
2022-02-11 00:11:02 +0000

    updated project description

--- src/components/projectdata.js
+++ src/components/projectdata.js
@@ -258,7 +258,7 @@ export const projectData = [
   {
     title: 'Inventory App',
     description:
-      'Full stack inventory management application built with express with a noSQL database with CRUD features. Users are able to create and update both categories and products. File upload is also possible using the Multer API. All user inputs are sanitized and validated using express-validator.',
+      'Full stack inventory management application built using express with a noSQL database enabling all CRUD features. Users are able to create and update both categories and products. File upload is also possible using the Multer API. All user inputs are sanitized and validated using express-validator.',
     images: [ia1, ia2, ia3, ia4],
     repo: 'https://github.com/sultanp91/inventory-app',
     live: 'https://floating-caverns-88055.herokuapp.com',
